4a4a6c0878063da07b2df53991d4884aa745eed8
Stanislav Soharev <devsoharev@yandex.by>
2023-01-14 19:27:14 +0300

    hw1. Implementation save(), get(), delete(), clear(), getAll(), size() in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,60 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int size = size();
+        if (size < storage.length) {
+            storage[size] = r;
+        } else {
+            System.out.println("Storage full");
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size - 1; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                storage[size - 1] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = size();
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        for (; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+        }
+        return i;
     }
 }
