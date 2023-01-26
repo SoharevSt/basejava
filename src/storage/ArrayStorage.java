0981b60dc2f69de33bd4db3b9e958adfb3ba8154
Stanislav Soharev <devsoharev@yandex.by>
2023-01-26 09:51:43 +0300

    hw3. Changing the project according to commits

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -7,71 +7,51 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private final int FULL_SIZE = 10000;
-    private int size = 0;
-    private final Resume[] storage = new Resume[FULL_SIZE];
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void save(Resume r) {
+    public void update(Resume r) {
         int index = getIndex(r.getUuid());
-        if (size == FULL_SIZE) {
-            System.out.println("ERROR: Storage full");
-        } else if (index != -1) {
-            System.out.println("ERROR: Resume with uuid " + r.getUuid() + " is already in storage");
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
         } else {
             storage[size] = r;
             size++;
         }
     }
 
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-        } else {
-            System.out.println("ERROR: Resume " + r.getUuid() + " missing from the storage");
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            return storage[index];
-        }
-        System.out.println("ERROR: Resume " + uuid + " missing from the storage");
-        return null;
-    }
-
     public void delete(String uuid) {
         int index = getIndex(uuid);
-        if (index != -1) {
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
-        } else {
-            System.out.println("ERROR: Resume " + uuid + " missing from the storage");
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    public int size() {
-        return size;
-    }
-
-    private int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if(storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
