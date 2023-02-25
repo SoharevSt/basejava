e909c470956767d73379332fc72f69a60fcc6e05
Stanislav Soharev <devsoharev@yandex.by>
2023-02-25 20:00:24 +0300

    hw6. Modified equals and hashCode in Resume

--- src/model/Resume.java
+++ src/model/Resume.java
@@ -38,12 +38,15 @@ public class Resume implements Comparable<Resume> {
 
         Resume resume = (Resume) o;
 
-        return uuid.equals(resume.uuid);
+        if (!uuid.equals(resume.uuid)) return false;
+        return fullName.equals(resume.fullName);
     }
 
     @Override
     public int hashCode() {
-        return uuid.hashCode();
+        int result = uuid.hashCode();
+        result = 31 * result + fullName.hashCode();
+        return result;
     }
 
     @Override
